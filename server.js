1cb8abd2f745d1f53e9327340552e08b3f2678a1
Jeong-hwan Kim <scotia901@gmail.com>
2023-04-23 07:36:56 +0900

    add https listen

--- server.js
+++ server.js
@@ -77,7 +77,12 @@ async function main () {
     app.use('/categories', categoryRoutes);
     app.use('/tasks', taskRoutes);
     app.use('/auth', authRoutes);
-    app.listen(process.env.SERVER_PORT, () => {
+    app.listen(process.env.SERVER_HTTP_PORT, () => {
+        const appOpenTime = new Date();
+        console.log(`App open on ${appOpenTime.toLocaleString()} and listening on Port:${process.env.SERVER_PORT}`);
+    });
+
+    app.listen(process.env.SERVER_HTTPS_PORT, () => {
         const appOpenTime = new Date();
         console.log(`App open on ${appOpenTime.toLocaleString()} and listening on Port:${process.env.SERVER_PORT}`);
     });
